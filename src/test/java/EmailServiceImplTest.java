ffb054ec1a98216943dd3daeb4cffbd34d05c248
LW611273 <luigi.woodhouse@digicelgroup.com>
2023-08-23 14:42:51 -0500

    added a unit test

--- src/test/java/EmailServiceImplTest.java
+++ src/test/java/EmailServiceImplTest.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import java.io.UnsupportedEncodingException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 import static org.mockito.Mockito.*;
 
 public class EmailServiceImplTest {
@@ -27,6 +28,8 @@ public class EmailServiceImplTest {
     public void setup() {
         MockitoAnnotations.initMocks(this);
     }
+
+    EmailServiceImpl emailServiceImplMock =  mock(EmailServiceImpl.class);
 
     @Test
     public void send_Payment_Successful_Email() throws MessagingException, UnsupportedEncodingException {
@@ -49,5 +52,28 @@ public class EmailServiceImplTest {
 
         // Ensure that the method returns the same customer object
         assertEquals(customer, result);
+    }
+
+    @Test
+    public void send_Payment_Email_Return_500() throws MessagingException, UnsupportedEncodingException {
+
+        Customer customer = new Customer();
+        customer.setName("John Doe");
+        customer.setEmail("johndoe@example.com");
+
+        MimeMessage mimeMessageMock = mock(MimeMessage.class);
+        MimeMessageHelper mimeMessageHelperMock = mock(MimeMessageHelper.class);
+
+        when(javaMailSender.createMimeMessage()).thenReturn(mimeMessageMock);
+        doNothing().when(mimeMessageHelperMock).setFrom(anyString(), anyString());
+
+        // Act
+        Customer result = emailServiceImpl.sendPaymentSuccessfulEmail(customer);
+
+        Exception exception = assertThrows(RuntimeException.class, ()
+                -> emailServiceImplMock.sendPaymentSuccessfulEmail(customer));
+
+        // Perform your assertions on the exception or any other expected behavior
+        assertEquals("Failed to send verification email", exception.getMessage());
     }
 }
